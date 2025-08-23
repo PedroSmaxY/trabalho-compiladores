439f219dba73060c3e24ff5e71a676049595a755
PedroSmaxY <pedrohnovais@outlook.com>
2025-08-23 10:09:59 -0300

    Commit inicial da estrutura do projeto

---
+++ include/lexer.h
@@ -0,0 +1,8 @@
+//
+// Created by Pedro Novais on 23/08/25.
+//
+
+#ifndef TRABALHO_LEXER_H
+#define TRABALHO_LEXER_H
+
+#endif //TRABALHO_LEXER_H
---
+++ src/lexer.c
@@ -0,0 +1,5 @@
+//
+// Created by Pedro Novais on 23/08/25.
+//
+
+#include "../include/lexer.h"
