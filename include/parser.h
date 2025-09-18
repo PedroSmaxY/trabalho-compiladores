a58b545bb671a24e93040001a21a97cf17ef157f
PedroSmaxY <pedrohnovais@outlook.com>
2025-09-18 14:20:32 -0300

    adicionando biblioteca parser e ajustando CMakeLists.txt para vinculação

---
+++ include/parser.h
@@ -0,0 +1,8 @@
+//
+// Created by Pedro Novais on 18/09/25.
+//
+
+#ifndef TRABALHO_COMPILADORES
+#define TRABALHO_COMPILADORES
+
+#endif //TRABALHO_COMPILADORES
---
+++ src/parser.c
@@ -0,0 +1,5 @@
+//
+// Created by Pedro Novais on 18/09/25.
+//
+
+#include "parser.h"
