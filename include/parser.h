8de6a96dd8ef8be752bd0ca0bb3dfd798266fe3f
PedroSmaxY <pedrohnovais@outlook.com>
2025-09-18 15:30:09 -0300

    atualizando definições de tipos e estrutura do nó da árvore sintática no parser

--- include/parser.h
+++ include/parser.h
@@ -10,21 +10,55 @@
 #include "utils.h"
 #include <stdio.h>
 
-typedef enum { ENodeStatement, ENodeExpression } ENodeKind;
-typedef enum { EStmtIf, EStmtRepeat, EStmtAssign, EStmtRead, EStmtWrite } EStatementKind;
-typedef enum { EExprOp, EExprConst, EExprId } EExpressionKind;
-typedef enum { ETypeVoid, ETypeInteger, ETypeBoolean } EExpressionType;
+typedef enum {
+    ENodeDeclaration,   // Declarações de variáveis (inteiro, real)
+    ENodeStatement,     // Comandos (if, while, etc.)
+    ENodeExpression     // Expressões (aritméticas, relacionais, lógicas)
+} ENodeKind;
+
+typedef enum {
+    EStmtIf,        // se-entao-senao
+    EStmtWhile,     // enquanto
+    EStmtRepeat,    // repita-ate
+    EStmtAssign,    // atribuição
+    EStmtRead,      // ler
+    EStmtWrite,     // mostrar
+    EStmtBlock      // bloco { ... }
+} EStatementKind;
+
+typedef enum {
+    EExprOp,        // Operador (+, -, *, /, <, ==, etc.)
+    EExprConst,     // Constante numérica
+    EExprId         // Identificador (variável)
+} EExpressionKind;
+
+typedef enum {
+    ETypeVoid,      // usado em comandos sem valor de retorno
+    ETypeInteger,   // inteiro (variável e expressão)
+    ETypeReal,      // real (variável e expressão)
+    ETypeBoolean    // resultado de expressões relacionais/lógicas
+} EExpressionType;
 
 typedef struct syntaxTreeNode {
-    struct syntaxTreeNode* children[MAX_CHILDREN];
-    struct syntaxTreeNode* nextSibling;
-    int lineNumber;
-    ENodeKind nodeKind;
-    union { EStatementKind stmt; EExpressionKind expr; } kind;
-    union { ETokenType op; int value; char *identifier; } attribute;
-    EExpressionType type;
+    struct syntaxTreeNode* children[MAX_CHILDREN];  // filhos diretos
+    struct syntaxTreeNode* nextSibling;             // próximo irmão (lista encadeada)
+    int lineNumber;                                 // número da linha do código fonte
+    ENodeKind nodeKind;                             // tipo de nó (declaração, comando, expressão)
+
+    union {
+        EStatementKind stmt;
+        EExpressionKind expr;
+    } kind;
+
+    union {
+        ETokenType op;      // operador (quando for expressão com operador)
+        int value;          // valor constante
+        char *identifier;   // nome de variável
+    } attribute;
+
+    EExpressionType type;   // tipo do nó (void, inteiro, real)
 } SyntaxTreeNode;
 
-void parse(FILE *fp);
+SyntaxTreeNode* parse(FILE *fp);
 
 #endif //TRABALHO_COMPILADORES
