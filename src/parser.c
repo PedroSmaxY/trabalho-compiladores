a9589865c687b3f1329b101ce0b690a6d40f85ef
PedroSmaxY <pedrohnovais@outlook.com>
2025-09-18 15:10:37 -0300

    adicionando função de análise no parser e incluindo bibliotecas necessárias

--- src/parser.c
+++ src/parser.c
@@ -2,4 +2,17 @@
 // Created by Pedro Novais on 18/09/25.
 //
 
+
 #include "parser.h"
+#include "lexer.h"
+#include "utils.h"
+
+void parse(FILE *fp) {
+    Token token = getToken(fp);
+    while (token.type != TOKEN_EOF) {
+        printf("Token: %s, Lexeme: %s, Line: %d\n", tokenTypeToString(token.type), token.lexeme, token.line);
+        token = getToken(fp);
+    }
+    // Placeholder for parser implementation
+    printf("Parsing the input file...\n");
+}
