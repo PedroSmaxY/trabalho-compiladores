8ab5fd43ed9e075f4198f253149bf33cef376c8f
PedroSmaxY <pedrohnovais@outlook.com>
2025-09-18 22:26:00 -0300

    Preparando output lexico

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 extern FILE *yyin;
 extern Node *ast_root;
 
+#define SHOW_LEXICAL_LOGS 0
+#define SHOW_PARSER_LOGS 1
+
 int main(const int argc, char **argv) {
     yyin = fopen(argc > 1 ? argv[1] : "codigo.txt", "r");
     if (!yyin) {
@@ -18,13 +21,20 @@ int main(const int argc, char **argv) {
 
     printf("Analise sintatica concluida.\n");
 
-    if (ast_root != NULL) {
-        printf("\n--- Saida Semantica ---\n");
-        printSemanticAst(ast_root, 0);
-        printf("----------------------------------------\n");
-    } else {
-        printf("Nao foi possivel gerar a arvore sintatica devido a erros.\n");
+    if (SHOW_LEXICAL_LOGS) {
+
     }
+
+    if (SHOW_PARSER_LOGS) {
+        if (ast_root != NULL) {
+            printf("\n--- Saida Semantica ---\n");
+            printSemanticAst(ast_root, 0);
+            printf("----------------------------------------\n");
+        } else {
+            printf("Nao foi possivel gerar a arvore sintatica devido a erros.\n");
+        }
+    }
+
 
     fclose(yyin);
     return 0;
